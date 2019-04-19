de1e260ce5252d0c207131043b29378d59340896
ganesh123reddy <giridharganesh63@gmail.com>
2019-04-19 22:06:04 +0530

    updated Code

--- apps/Faculty/urls.py
+++ apps/Faculty/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url,include
 from . import views
 urlpatterns = [
 	url(r'^$', views.faculty),
+	url(r'apply/',views.applypage),
+
 ]
--- apps/Faculty/views.py
+++ apps/Faculty/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from ..register.models import User
 # Create your views here.
 def faculty(request):
-    user = User.objects.get(email=request.User.email)
+    user = User.objects.get(email=request.session['email'])
     context = {
         "user": user
     }
-    return render(request, 'faculty/faculty_home.html', context)
+    return render(request, 'register/faculty_home.html', context)
+
+def applypage(request):
+	return render(request,'register/apply.html')
+
+def checkapply(request):
+	if request.method == "POST":
