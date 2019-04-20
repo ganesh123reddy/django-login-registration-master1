67af46a1dac35dc42609b914a7c651dd3f590554
ganesh123reddy <giridharganesh63@gmail.com>
2019-04-20 17:33:42 +0530

    Update views.py

--- apps/Faculty/views.py
+++ apps/Faculty/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from ..register.models import User
+from django.shortcuts import render, HttpResponse, redirect
 # Create your views here.
 def faculty(request):
     user = User.objects.get(email=request.session['email'])
@@ -12,10 +13,10 @@ def applypage(request):
 	return render(request,'register/apply.html')
 
 def checkapply(request):
-	if request.method == "POST":
+	if True:
 		#email=request.session['email']
 		user = User.objects.get(email=request.session['email'])
-		if request.POST['leave_type']=="2":
+		if request.POST['leave_type']==2:
 			if user.n_compleaves>0:
 				user.n_casualleaves=user.n_compleaves-1
 				user.u_casualleave=user.u_casualleave+1
@@ -23,7 +24,9 @@ def checkapply(request):
 				context={
 				"user":user
 				}
-				print("Entered and succeded")
-				#return render(request,'register/faculty_home.html',context)
-
+				#print("Entered and succeded")
+				return render(request,'register/faculty_home.html',context)
+		else:
+			return redirect('/')
+				
 
