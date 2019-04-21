8143498d2c38c4c4271588f02ecac2ccb9b32be0
ganesh123reddy <giridharganesh63@gmail.com>
2019-04-21 21:54:34 +0530

    gg

--- apps/HOD/views.py
+++ apps/HOD/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from ..register.models import User
+from ..Faculty.models import Leaves
 # Create your views here.
 def hod(request):
 	return render(request,"register/hod_home.html")
 
-
 def approvepage(request):
-	return render(request,"register/singleapprove.html")
+	leave=Leaves.objects.all()
+	"""l1=[]
+				for l in leave:
+					l1.append(l.email)
+				print(l1)	"""
+
+	context={
+	"Leaves":leave
+	}
+	return render(request,"register/approve.html",context)
 
 def approvecheck(request):
 	return -1
+
