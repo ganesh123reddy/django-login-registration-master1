3aed7789016644be98f37074daa6a800df5af5d5
ganesh123reddy <giridharganesh63@gmail.com>
2019-04-22 15:03:24 +0530

    hh

--- apps/Director/views.py
+++ apps/Director/views.py
@@ -9,43 +9,50 @@ def director(request):
 	    context = {
 	        "user": user
 	    }
-	    if user.
-	    return render(request, 'register/director_home.html', context)
+	    if user.user_type == "director":
+	    	return render(request, 'register/director_home.html', context)
+	    else:
+	    	return redirect('/')		
+	    			
 	else:	
 		return redirect('/')
 
 
 def approvepage_dir(request):
 	#print("*********************************************")
-	leave=Leaves.objects.all()
-	context={
-	"Leaves":leave
-	}
-	return render(request,"register/approve1.html",context)
+	if request.session['email'] != None:
+		leave=Leaves.objects.all()
+		context={
+		"Leaves":leave
+		}
+		return render(request,"register/approve1.html",context)
+	else:
+		return redirect('/')	
 
 
 
 #from dateutil.parser import parse
 def singledetails_dir(request):
 	#if request.method=='POST' and User.objects.get(email=request.POST['email']):
-	print("*********************************************")
-	if request.POST.get('Accept'):
-		email=request.POST.get('email')
-		lt=request.POST.get('Type')
-		fd=request.POST.get('fromdate')
-		td=request.POST.get('todate')
-		l=Leaves.objects.get(email=email,leave_type=lt)
-		l.status=1
-		l.save()
-		return render(request,"register/director_home.html") 
-		#return render(request,"register/singleapprove.html",context)
-	else:
-		email=request.POST.get('email')
-		lt=request.POST.get('Type')
-		fd=request.POST.get('fromdate')
-		td=request.POST.get('todate')
-		l=Leaves.objects.get(email=email,leave_type=lt)
-		l.status=0
-		l.save()
-		return render(request,"register/director_home.html")
+	#print("*********************************************")
+	if request.session['email'] != None:
+		if request.POST.get('Accept'):
+			email=request.POST.get('email')
+			lt=request.POST.get('Type')
+			fd=request.POST.get('fromdate')
+			td=request.POST.get('todate')
+			l=Leaves.objects.get(email=email,leave_type=lt)
+			l.status=1
+			l.save()
+			return render(request,"register/director_home.html") 
+			#return render(request,"register/singleapprove.html",context)
+		else:
+			email=request.POST.get('email')
+			lt=request.POST.get('Type')
+			fd=request.POST.get('fromdate')
+			td=request.POST.get('todate')
+			l=Leaves.objects.get(email=email,leave_type=lt)
+			l.status=0
+			l.save()
+			return render(request,"register/director_home.html")
 
